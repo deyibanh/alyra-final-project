0e5302b5803c2906f02617cbdc339dc7a62cb2ae
MB2M <damsbooster@gmail.com>
2021-12-21 15:43:56 +0100

    create DeliveryMaster and some functionalities (no testing)

---
+++ contracts/interfaces/IDeliveryMaster.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.9;
+
+interface IDeliveryMaster {
+    enum DeliveryState {
+        noInfo,
+        registered,
+        atHub,
+        inDelivery,
+        arrived,
+        delivered
+    }
+
+    struct CommercialData {
+        string client;
+        uint256 price;
+        uint256 hubID;
+    }
+
+    struct Delivery {
+        DeliveryState state;
+        CommercialData commercialData;
+    }
+
+    function newDelivery(
+        string memory _client,
+        uint256 _price,
+        uint256 _hubID
+    ) external;
+
+    function getDelivery(uint256 _deliveryID)
+        external
+        view
+        returns (Delivery memory);
+
+    function setDeliveryState(uint256 _deliveryID, DeliveryState _deliveryState)
+        external
+        returns (bool);
+}
