d4cf01f6496a0ecec4ee95e183a68fa0dfafe0d3
Dé Yi Banh <deyi.banh@gmail.com>
2021-12-27 11:10:54 +0100

    refactor(eslint): Add eslint in client
    Add eslint file.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./App";
 import reportWebVitals from "./utils/reportWebVitals";
 import * as serviceWorker from "./utils/serviceWorker";
 import "./index.css";
@@ -9,10 +9,10 @@ import "./index.css";
 // import 'bootstrap/dist/js/bootstrap.min.js';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
